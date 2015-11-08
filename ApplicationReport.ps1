d433f2ea444fecb0b3f0e4d32c35ef76580d0b1a
Matt <matt.egan@varian.com>
2015-11-07 18:24:55 -0700

    initial commit

---
+++ ApplicationReport.ps1
@@ -0,0 +1,50 @@
+﻿Import-Module Okta
+$oktaVerbose = $true
+$org = 'prod'
+
+#Get all the active Apps within the org
+$activeapps = oktaGetActiveApps -oOrg $org
+
+$col1 = New-Object System.Collections.ArrayList
+foreach ($app in $activeapps)
+{
+    #fetch the groups that are used to assign the app to users
+    $appgroups = oktaGetAppGroups -oOrg $org -AppId $app.id
+    foreach($group in $appgroups)
+    {
+        try
+        {
+            #fetch the details of the group
+            $oktagroup = oktaGetGroupbyId -oOrg $org -groupId $group.id
+        }
+        catch
+        {
+            Write-Error "group didn't exist?"
+        }
+
+        $params = @{ Application = $app
+                     Priority = $group.priority
+                     Updated = $group.lastUpdated
+                     Group = $oktagroup
+                   }
+        $_c = $col1.add($params)
+    }
+}
+
+$col2 = new-object System.Collections.ArrayList
+foreach ($c in $col1)
+{
+    $param = @{ Application = $c.Application.name
+                AppDesc= $c.Application.label
+                AppUserNameTemplate = $c.Application.credentials.userNameTemplate.template
+                AssignmentPriority = $c.Priority
+                GroupClass = $c.Group.ObjectClass[0]
+                GroupName = $C.Group.profile.name
+                GroupDesc = $C.Group.profile.description
+                GroupID = $C.Group.id
+              }
+    $row = New-Object psobject -Property $param
+    $_c  = $col2.add($row)
+}
+
+$col2 | Export-Csv -Path OktaAppReport.csv -NoTypeInformation
---
+++ RolesAudit.ps1
@@ -0,0 +1,51 @@
+﻿$oktaVerbose = $true
+$org = 'prod'
+
+#Get the ID of your admin application, easiest way i know is to run a report
+#filter the report by 'Okta Administration' the appid is printed along with every action
+#cheat like I do an expand your okta org definition to keep quick tabs on it 
+$adminAppID = $oktaOrgs.$org.AdminAID
+
+#Collect a list of users that are assigned to the admin application
+$admins = oktaGetUsersbyAppID -oOrg prod -aid $oktaOrgs.prod.AdminAID
+
+$byUser = New-Object System.Collections.ArrayList
+
+#loop through the admins, retrieve their role(s)
+foreach ($a in $admins)
+{
+    $user = oktaGetUserbyID -oOrg prod -userName $a.id
+    $roles = oktaGetRolesByUserId -oOrg prod -uid $a.id
+    Add-Member -InputObject $user -MemberType NoteProperty -Name roles -Value $roles
+
+    $_c = $byUser.Add($user)
+}
+
+#create a report
+$col1 = New-Object System.Collections.ArrayList
+
+foreach ($user in $byUser)
+{
+    foreach ($role in $user.roles)
+    {
+        $param = @{ role = $role.label
+                    type = $role.type
+                    UserRoleCreated = $role.created
+                    UserRoleupdated = $role.lastUpdated
+                    userCreated = $user.created
+                    userUpdated = $user.lastUpdated
+                    userLastLogin = $user.lastLogin
+                    userCredsType = $user.credentials.provider.type
+                    userCredsProv = $user.credentials.provider.name
+                    status = $role.status
+                    userName = $user.profile.login
+                    firstName = $user.profile.firstName
+                    lastName = $user.profile.lastName
+                    userid = $user.id
+                  }
+        $row = New-Object psobject -Property $param
+        $_c = $col1.Add($row)
+    }
+}
+
+$col1 | Export-Csv -Path OktaAdminReport.csv -NoTypeInformation
